7da73d4109be9829d4f34f7779974129dbc3b0f8
gdlrb <denisgalynsky@gmail.com>
2017-05-03 11:55:00 +0300

    battle v 0.0.1

--- Games/src/app/battleship/battleship.component.ts
+++ Games/src/app/battleship/battleship.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input, ViewChild, ElementRef, ViewEncapsulation } from '@angular/core';
+import * as $ from 'jquery';
 
 import { Cage } from 'app/cage';
 
@@ -63,13 +64,27 @@ export class BattleshipComponent implements OnInit {
     this.buildUIField(this.elementRef.nativeElement.querySelector('#player-ships'), this.game.playerShips);
     this.buildUIField(this.elementRef.nativeElement.querySelector('#bot-game'), this.game.botGame);
     this.buildUIField(this.elementRef.nativeElement.querySelector('#bot-ships'), this.game.botShips);
+    const those = this;
+
+    $('#player-game').on('click', '.cell', function () {
+      those.handleClick($(this)); console.log($(this));
+
+      const r = Math.floor(Math.random() * 99);
+      const all = $('#bot-game  cell');
+      for (let b = 0; b < r; b++) {
+        var botCells = [];
+        botCells.push(all[b]);
+      }
+      those.botClick($(botCells)); console.log($(botCells));
+    });
   }
 
+
   public buildEmptyField(): any {
-    let field = [];
+    const field = [];
 
     for (let y = 0; y < Cage.MAX_Y; y++) {
-      let row = [];
+      const row = [];
 
       for (let x = 0; x < Cage.MAX_X; x++) {
         row.push(this.CELL_CLEAR);
@@ -115,7 +130,7 @@ export class BattleshipComponent implements OnInit {
           let targetY;
           for (let c = 0; c < decks; c++) {
             targetX = x + c * xOffset;
-            targetY = x + c * yOffset;
+            targetY = y + c * yOffset;
             result = this.isClearAround(battlefield, targetX, targetY);
             if (!result) {
               break;
@@ -125,7 +140,7 @@ export class BattleshipComponent implements OnInit {
           if (result) {
             for (let c = 0; c < decks; c++) {
               targetX = x + c * xOffset;
-              targetY = x + c * yOffset;
+              targetY = y + c * yOffset;
               battlefield[targetX][targetY] = this.CELL_WITH_SHIP;
             }
 
@@ -176,6 +191,205 @@ export class BattleshipComponent implements OnInit {
     }
   }
 
+  public handleClick(cell): any {
+    let c = cell.attr('data-coord');
+    if (!c) {
+      return;
+    }
+    c = JSON.parse(c);
+    const status = this.game.playerGame[c.x][c.y];
+    if (status !== this.CELL_CLEAR) {
+      return;
+    }
 
+    const target = this.game.botShips[c.x][c.y];
+    let new_status = -1;
+    switch (target) {
+      case this.CELL_WITH_SHIP:
+        new_status = this.CELL_DECK_BROKEN;
+        break;
+      case this.CELL_CLEAR:
+        new_status = this.CELL_MISS_SHOT;
+        break;
+    }
+    if (new_status === -1) {
+      return;
+    }
+    this.game.botShips[c.x][c.y] = new_status;
 
+    if (new_status === this.CELL_DECK_BROKEN) {
+      const result = this.handleShipCrash(this.game.botShips, c.x, c.y);
+      if (result !== false) {
+        // Убит корабль противника - найти пространство которое нужно покрасить
+        let minX = 10, maxX = 0, minY = 10, maxY = 0;
+        for (let i = 0; i < result.length; i++) {
+          minX = Math.min(minX, result[i].x);
+          maxX = Math.max(maxX, result[i].x);
+          minY = Math.min(minY, result[i].y);
+          maxY = Math.max(maxY, result[i].y);
+        }
+        // Красим поле игрока
+        for (let x = minX; x <= maxX; x++) {
+          for (let y = minY; y <= maxY; y++) {
+            let newPlayerCellStatus = -1;
+            switch (this.game.botShips[x][y]) {
+              case this.CELL_CLEAR:
+                newPlayerCellStatus = this.CELL_NO_SENSE;
+                break;
+            }
+            if (newPlayerCellStatus !== -1) {
+              this.game.playerGame[x][y] = newPlayerCellStatus;
+              const coordinate = '{"x":' + x + ',"y":' + y + '}';
+              const tcell = cell.closest('.t').find('div[data-coord=\'' + coordinate + '\']');
+              this.colorizeCell(tcell, newPlayerCellStatus);
+            }
+          }
+        }
+      }
+    }
+    this.colorizeCell(cell, new_status);
+  }
+
+  public botClick(cell): any {
+    let c = cell.attr('data-coord');
+    if (!c) {
+      return;
+    }
+    c = JSON.parse(c);
+
+    const status = this.game.botGame[c.x][c.y];
+    if (status !== this.CELL_CLEAR) {
+      return;
+    }
+
+    const target = this.game.playerShips[c.x][c.y];
+    let new_status = -1;
+    switch (target) {
+      case this.CELL_WITH_SHIP:
+        new_status = this.CELL_DECK_BROKEN;
+        break;
+      case this.CELL_CLEAR:
+        new_status = this.CELL_MISS_SHOT;
+        break;
+    }
+    if (new_status === -1) {
+      return;
+    }
+    this.game.playerShips[c.x][c.y] = new_status;
+
+    if (new_status === this.CELL_DECK_BROKEN) {
+      const result = this.handleShipCrash(this.game.playerShips, c.x, c.y);
+      if (result !== false) {
+        // Убит корабль противника - найти пространство которое нужно покрасить
+        let minX = 10, maxX = 0, minY = 10, maxY = 0;
+        for (let i = 0; i < result.length; i++) {
+          minX = Math.min(minX, result[i].x);
+          maxX = Math.max(maxX, result[i].x);
+          minY = Math.min(minY, result[i].y);
+          maxY = Math.max(maxY, result[i].y);
+        }
+        // Красим поле бота
+        for (let x = minX; x <= maxX; x++) {
+          for (let y = minY; y <= maxY; y++) {
+            let newBotCellStatus = -1;
+            switch (this.game.playerShips[x][y]) {
+              case this.CELL_CLEAR:
+                newBotCellStatus = this.CELL_NO_SENSE;
+                break;
+            }
+            if (newBotCellStatus !== -1) {
+              this.game.botGame[x][y] = newBotCellStatus;
+              const coordinate = '{"x":' + x + ',"y":' + y + '}';
+              const tcell = cell.closest('.t').find('div[data-coord=\'' + coordinate + '\']');
+              this.colorizeCell(tcell, newBotCellStatus);
+            }
+          }
+        }
+      }
+    }
+    this.colorizeCell(cell, new_status);
+  }
+
+  // Проверяем на поле - убит ли корабль и если убит - позвращаем список окружающих корабль координат
+  // Приходит поле с кораблями и координата успешного выстрела
+  public handleShipCrash(field, x, y) {
+    let i;
+    let hasNotBrokenDeck = false;
+    const cells = [];
+    // По горизонтали вправо пока не край или не пустое поле
+    for (i = x + 1; i <= x + 4; i++) {
+      if (typeof (field[i]) === 'undefined' || typeof (field[i][y]) === 'undefined') {
+        // Вышли за поле
+        continue;
+      }
+      if (field[i][y] === this.CELL_WITH_SHIP) {
+        // Есть целая палуба - дальше нечего проверять - корабль не уничтожен
+        hasNotBrokenDeck = true;
+        break;
+      }
+      // запишем эту ячейку
+      cells.push({
+        x: i,
+        y: y,
+        s: field[i][y]
+      });
+      // Здесь все что угодно кроме подбитой палубы
+      if (field[i][y] !== this.CELL_DECK_BROKEN) {
+        break;
+      }
+    }
+    // По горизонтали влево
+    for (i = x - 1; i >= x - 4; i--) {
+      if (typeof (field[i]) === 'undefined' || typeof (field[i][y]) === 'undefined') {
+        continue;
+      }
+      if (field[i][y] === this.CELL_WITH_SHIP) {
+        hasNotBrokenDeck = true;
+        break;
+      }
+      cells.push({ x: i, y: y, s: field[i][y] });
+      if (field[i][y] !== this.CELL_DECK_BROKEN) {
+        break;
+      }
+    }
+    // По вертикали вверх
+    for (i = y - 1; i >= y - 4; i--) {
+      if (typeof (field[x]) === 'undefined' || typeof (field[x][i]) === 'undefined') {
+        continue;
+      }
+      if (field[x][i] === this.CELL_WITH_SHIP) {
+        hasNotBrokenDeck = true;
+        break;
+      }
+      cells.push({ x: x, y: i, s: field[x][i] });
+      if (field[x][i] !== this.CELL_DECK_BROKEN) {
+        break;
+      }
+    }
+    // По вертикал вниз
+    for (i = y + 1; i <= y + 4; i++) {
+      if (typeof (field[x]) === 'undefined' || typeof (field[x][i]) === 'undefined') {
+        continue;
+      }
+      if (field[x][i] === this.CELL_WITH_SHIP) {
+        hasNotBrokenDeck = true;
+        break;
+      }
+      cells.push({ x: x, y: i, s: field[x][i] });
+      if (field[x][i] !== this.CELL_DECK_BROKEN) {
+        break;
+      }
+    }
+    if (hasNotBrokenDeck) {
+      // Не обрабатывем. Этот корабль не убит
+      return false;
+    }
+    return cells;
+  }
+
+  public colorizeCell(cell, status) {
+    cell.removeClass(function (index, className) {
+      return (className.match(/(^|\s)cell_-\S+/g) || []).join(' ');
+    }).addClass(this.statusClass[status]);
+  }
 }
--- Games/src/app/cage.ts
+++ Games/src/app/cage.ts
@@ -12,19 +12,13 @@ export class Cage {
     private _y: number;
 
     constructor(x: number, y: number) {
-        if (x < 0) {
-            throw new Error('X must be above zero!');
-        }
-        if (x > Cage.MAX_X) {
-            throw new Error('X must not be below "1"!');
+        if (x < 10) {
+            throw new Error('X < 10!');
         }
         this._x = Math.round(x);
 
-        if (y < 0) {
-            throw new Error('Y must be above zero!');
-        }
-        if (y > Cage.MAX_Y) {
-            throw new Error('Y must not be below "1"!');
+        if (y < 10) {
+            throw new Error('Y < 10!');
         }
         this._y = Math.round(y);
     }
